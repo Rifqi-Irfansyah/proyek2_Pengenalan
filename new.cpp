0f3ae0d600741bf5b0373ece854aafffd1092fac
Alqan Nazra <alqan.nazra.tif23@polban.ac.id>
2024-02-12 20:21:29 +0700

    Nambah for

--- new.cpp
+++ new.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 int main ()
 {
+    float a[10];
+    cout << "Enter 5 numbers: ";
+    for (int i = 0 ; i < 5 ; i++) {
+        cin >> a[i] ;
+    }
     cout << "hallo world";
 }
